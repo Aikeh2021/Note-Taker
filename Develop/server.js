53ec569b8730acb9e526dcd7ed56b2611342306d
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-01-03 17:56:31 -0500

    Added middleware.

--- Develop/server.js
+++ Develop/server.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+//Middleware
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 //Listening in on the PORT
 
 app.listen(PORT, () => {
