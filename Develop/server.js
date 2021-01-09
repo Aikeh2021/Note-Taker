f1a378eb83c84388b5b17617887b3eb1e09ea920
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-01-08 22:07:20 -0500

    Removed the asterick from the get route for the index and replaced it with /index.

--- Develop/server.js
+++ Develop/server.js
@@ -32,7 +32,7 @@ app.get("/api/config", (req, res) => {
 });
 
 //View Routes
-app.get("/", (req, res) => {
+app.get("/index", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 app.get("/notes", (req, res) => {
