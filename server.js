f20ff4c16a64641631a667e8bb71685730b03cfa
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-01-17 16:43:42 -0500

    Still trying to figure out how to get newly submitted notes into the existing array on the db.json file.

--- server.js
+++ server.js
@@ -41,29 +41,30 @@ app.get("/api/notes", (req, res) => {
 });
 app.post("/api/notes", (req, res) => {
     let note = req.body;
-    console.log(note);
+    // console.log(note);
+    let dbData = [];
     
 
-    //Reading the JSON file
-    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (error, jsonData) => {
-        if (jsonData ===""){
-            dbData = [];
+    //Reading the JSON file and writing to the db.json if there is no other data in the database
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (error, data) => {
+        if (data ===""){
             dbData.push(note);
             fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(dbData), (err) => {
                 err ? console.error(err) : res.json(dbData); console.log("The note has been saved to the database.");
             })
             console.log(error);
-        }
-        // try{
-        //     newData = [jsonData];
-        //     newData.push(note);
-        //     fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(newData), (err) =>{
-        //         err ? console.error(err) : res.json(newData);console.log("The note has been saved to the database.");
-        //     })
-        // }catch(err) {
-        //     console.log(err);
-        // }
+        } else{
+                // let currentData = data.slice(0);
+                // dbData.push(currentData, JSON.stringify(note));
+                dbData.push(data, JSON.stringify(note));
+            fs.writeFile(path.join(__dirname, "db/db.json"), dbData, (err) => {
+                err ? console.error(err) : data.push(res.json(dbData)); console.log("The note has been saved to the database.");
+            })
+        
+            console.log(error);
+        };
     });
+
 
 })
 
